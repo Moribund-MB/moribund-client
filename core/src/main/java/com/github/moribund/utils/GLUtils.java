30878d7baaba393ef53d0d388df293c104892441
Arham4 <arhamjsiddiqui@gmail.com>
2019-01-03 14:27:06 -0600

    changed background to be black by default

--- core/src/main/java/com/github/moribund/utils/GLUtils.java
+++ core/src/main/java/com/github/moribund/utils/GLUtils.java
@@ -13,7 +13,7 @@ public class GLUtils {
      * Clears the GL without revealing all the magical stuff behind-the-scenes.
      */
     public void clearGL() {
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 }
