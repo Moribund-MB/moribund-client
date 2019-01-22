3fb077da792249708ccf3949f2fca7600f645189
Arham4 <arhamjsiddiqui@gmail.com>
2019-01-21 20:23:34 -0600

    moved inventory UI to its respective Inventory class

--- core/src/main/java/com/github/moribund/objects/playable/players/containers/Inventory.java
+++ core/src/main/java/com/github/moribund/objects/playable/players/containers/Inventory.java
@@ -1,8 +1,12 @@
 package com.github.moribund.objects.playable.players.containers;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.github.moribund.graphics.SpriteContainer;
+import com.github.moribund.graphics.SpriteFile;
 import com.github.moribund.graphics.drawables.DrawableUIAsset;
+import lombok.val;
 
 public class Inventory extends ItemContainer implements DrawableUIAsset {
     public static final int SLOTS = 5;
@@ -18,6 +22,23 @@ public class Inventory extends ItemContainer implements DrawableUIAsset {
 
     @Override
     public void draw(SpriteBatch spriteBatch) {
+        val unselected = new Sprite[SLOTS];
+        val centeringConstant = 175; // todo find a more mathematical way to do this
+        for (int i = 0; i < unselected.length; i++) {
+            unselected[i] = new Sprite(SpriteContainer.getInstance().getSprite(SpriteFile.INVENTORY_UNSELECTED));
+            unselected[i].setX(Gdx.graphics.getWidth() - (unselected[i].getWidth() * (i + 1)) - centeringConstant);
+            unselected[i].setY(0);
+            unselected[i].setAlpha(0.8f);
+            unselected[i].draw(spriteBatch);
+        }
+        val selected = new Sprite[2];
+        for (int i = 0; i < selected.length; i++) {
+            selected[i] = new Sprite(SpriteContainer.getInstance().getSprite(SpriteFile.INVENTORY_SELECTED));
+            selected[i].setX(Gdx.graphics.getWidth() - (selected[i].getWidth() * (unselected.length + (i + 1))) - centeringConstant);
+            selected[i].setY(0);
+            selected[i].setAlpha(0.8f);
+            selected[i].draw(spriteBatch);
+        }
         for (int i = 0; i < items.size(); i++) {
             items.get(i).draw(i, spriteBatch);
         }
--- core/src/main/java/com/github/moribund/screens/game/GameScreen.java
+++ core/src/main/java/com/github/moribund/screens/game/GameScreen.java
@@ -1,6 +1,5 @@
 package com.github.moribund.screens.game;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
@@ -9,12 +8,10 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.github.moribund.MoribundClient;
 import com.github.moribund.graphics.SpriteContainer;
-import com.github.moribund.graphics.SpriteFile;
 import com.github.moribund.graphics.drawables.DrawableGameAsset;
 import com.github.moribund.objects.attributes.Collidable;
 import com.github.moribund.objects.attributes.Flaggable;
 import com.github.moribund.objects.playable.players.PlayableCharacter;
-import com.github.moribund.objects.playable.players.containers.Inventory;
 import com.github.moribund.utils.GLUtils;
 import lombok.val;
 
@@ -80,23 +77,6 @@ class GameScreen implements Screen {
     }
 
     private void drawUI() {
-        val unselected = new Sprite[Inventory.SLOTS];
-        val centeringConstant = 175; // todo find a more mathematical way to do this
-        for (int i = 0; i < unselected.length; i++) {
-            unselected[i] = new Sprite(SpriteContainer.getInstance().getSprite(SpriteFile.INVENTORY_UNSELECTED));
-            unselected[i].setX(Gdx.graphics.getWidth() - (unselected[i].getWidth() * (i + 1)) - centeringConstant);
-            unselected[i].setY(0);
-            unselected[i].setAlpha(0.8f);
-            unselected[i].draw(uiSpritebatch);
-        }
-        val selected = new Sprite[2];
-        for (int i = 0; i < selected.length; i++) {
-            selected[i] = new Sprite(SpriteContainer.getInstance().getSprite(SpriteFile.INVENTORY_SELECTED));
-            selected[i].setX(Gdx.graphics.getWidth() - (selected[i].getWidth() * (unselected.length + (i + 1))) - centeringConstant);
-            selected[i].setY(0);
-            selected[i].setAlpha(0.8f);
-            selected[i].draw(uiSpritebatch);
-        }
         MoribundClient.getInstance().getDrawableUIAssets().forEach(drawable -> drawable.draw(uiSpritebatch));
     }
 
