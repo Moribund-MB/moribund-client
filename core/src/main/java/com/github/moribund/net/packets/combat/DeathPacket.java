1249b12e1a0cc763abc0ea449398f50f41cb4703
aryouham <arhamjsiddiqui@gmail.com>
2019-01-24 14:37:01 -0600

    fixed problem with death packet

--- core/src/main/java/com/github/moribund/net/packets/combat/DeathPacket.java
+++ core/src/main/java/com/github/moribund/net/packets/combat/DeathPacket.java
@@ -1,11 +1,11 @@
 package com.github.moribund.net.packets.combat;
 
 import com.github.moribund.net.packets.IncomingPacket;
-import lombok.Value;
 
-@Value
 public class DeathPacket implements IncomingPacket {
     private int playerId;
+
+    private DeathPacket() { }
 
     @Override
     public void process() {
